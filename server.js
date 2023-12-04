29b72272e89b71b8816a31b69a298779637db241
Ilyas Assakali <ilyas.assakali@student.ehb.be>
2023-12-04 20:04:18 +0100

    project initialization

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
+const socket = require("socket.io");
+const io = socket(server);
+
+io.on("connection", (socket) => {
+  socket.emit("me", socket.id);
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("callEnded");
+  });
+
+  socket.on("callUser", (data) => {
+    io.to(data.userToCall).emit("callUser", {
+      signal: data.signalData,
+      from: data.from,
+      data: data.name,
+    });
+  });
+
+   
+
+});
+
+server.listen(4000, () => console.log("server running on port 4000"));
